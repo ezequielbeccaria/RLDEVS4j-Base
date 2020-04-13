2c0b9d1572d35984c711a4a249fe3cbf3f19ac66
Ezequiel <ezequielbeccaria@gmail.com>
2020-04-13 17:12:01 -0300

    Continuous actions update

---
+++ src/main/java/rldevs4j/base/env/msg/Continuous.java
@@ -0,0 +1,24 @@
+package rldevs4j.base.env.msg;
+
+/**     
+ * Multi-Categorical Event with parametric values of type T.
+ * 
+ * @author Ezequiel Beccaría
+ */
+public class Continuous extends Event { 
+    private final double[] value;    
+    
+    public Continuous(int id, String name, EventType type, double[] value) {
+        super(id, name, type);
+        this.value = value;
+        
+    }
+
+    public int size(){
+        return value.length;
+    }
+
+    public double[] getValue() {
+        return value;
+    }
+}
