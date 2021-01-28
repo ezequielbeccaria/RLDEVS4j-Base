37e07f796973be4289dfff6cfd41775993e8f980
Ezequiel <ezequielbeccaria@gmail.com>
2021-01-28 15:17:38 -0300

    Mod DummyAgent

--- src/main/java/rldevs4j/base/agent/DummyAgent.java
+++ src/main/java/rldevs4j/base/agent/DummyAgent.java
@@ -2,12 +2,9 @@
 package rldevs4j.base.agent;
 
 import rldevs4j.base.agent.preproc.Preprocessing;
-import rldevs4j.base.env.msg.Continuous;
 import rldevs4j.base.env.msg.Event;
-import rldevs4j.base.env.msg.EventType;
 import rldevs4j.base.env.msg.Step;
 
-import java.util.Arrays;
 
 /**
  *
@@ -15,36 +12,18 @@ import java.util.Arrays;
  */
 public class DummyAgent extends Agent{
     private double cumReward = 0D;
-    private int actionSize;
-    private boolean flag = true;
+    private final Event defaultAction;
 
 
-    public DummyAgent(String name, Preprocessing preprocessing, int actionSize) {
+    public DummyAgent(String name, Preprocessing preprocessing, Event defaultAction) {
         super(name, preprocessing, 1D);
-        this.actionSize = actionSize;
+        this.defaultAction = defaultAction;
     }
 
     @Override
     public Event observation(Step step) {
-        cumReward += step.getReward();
-
-        float[] a = new float[actionSize];
-        Arrays.fill(a, 0F);
-//        if(step.getFeature(-1) == 0D)
-//            a = new double[]{0D, 500D};
-//        if(step.getFeature(2)<1000 && flag){
-//            flag = false;
-//            a = new double[]{0D, 1000D};
-//        }
-
-        Continuous action = new Continuous(
-                100, 
-                "action", 
-                EventType.action,
-                a);
-//                new double[]{0D, (step.getFeature(-1)>30D && step.getFeature(-1)>35D)?1D:0D});
-        
-        return action;
+        cumReward += step.getReward();       
+        return defaultAction;
     }
 
     @Override
@@ -59,7 +38,6 @@ public class DummyAgent extends Agent{
 
     @Override
     public void clear() {
-        flag = true;
         cumReward = 0D;
     }
 
